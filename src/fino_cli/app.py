27e056249f45fbbed0544025872545a0c5dea911
Akiya-Kudo <k.akiya07@gmail.com>
2026-01-12 15:25:38 +0900

    colortheme回収

--- src/fino_cli/app.py
+++ src/fino_cli/app.py
@@ -1,10 +1,9 @@
-import fino_cli.command.collector.app as collector
+import fino_cli.collector.app as collector
 import typer
-from fino_cli.util.theme import FinoColors
-from rich.console import Console
+from fino_cli import test
+from fino_cli.util.theme import create_console
 from rich.panel import Panel
-
-console = Console()
+from rich.text import Text
 
 app = typer.Typer(
     name="fino",
@@ -20,30 +19,77 @@ app.add_typer(
     name="collector",
 )
 
+app.add_typer(test.app)
+
+
+ASCII_LOGO = r"""
+  /$$$$$$  /$$                                     /$$ /$$
+ /$$__  $$|__/                                    | $$|__/
+| $$  \__/ /$$ /$$£$$$$   /$$$$$$         /$$$$$$$| $$ /$$
+| $$$$    | $$| $$__  $$ /$$__  $$       /$$_____/| $¥| $$
+| $$_/    | $$| $$  \ $$| $$  \ $$      | $$      | $$| $$
+| $$      | $$| $$  | $$| $$  | $$      | $$      | $$| $$
+| $$      | $$| $$  | $$|  $$$€$$/      |  $$$$$$$| $$| $$
+|__/      |__/|__/  |__/ \______/        \_______/|__/|__/
+"""
+
 
 @app.callback(invoke_without_command=True)
-def callback(ctx: typer.Context) -> None:
-    # コマンドが指定されている場合はパネルを表示しない
+def root(ctx: typer.Context):
     if ctx.invoked_subcommand is not None:
         return
 
-    # コマンドなしで実行された場合のみオンボーディングパネルを表示
+    console = create_console()
+
+    fino_logo = Text(ASCII_LOGO)
+
+    logo_length = len(fino_logo)
+
+    # グラデーション風に色付け
+    fino_logo.stylize("brand.primary", 0, logo_length // 4)
+    fino_logo.stylize("brand.secondary", logo_length // 4, logo_length // 2)
+    fino_logo.stylize(
+        "brand.tertiary",
+        logo_length // 2,
+        logo_length * 3 // 4,
+    )
+    fino_logo.stylize("brand.quaternary", logo_length * 3 // 4, logo_length)
+
     console.print(
         Panel.fit(
-            f"""[bold {FinoColors.ORANGE3}]Fino CLI[/bold {FinoColors.ORANGE3}] 
-            - Financial data management CLI tool -
-            [bold {FinoColors.LIGHT_SALMON3}]Fino[/bold {FinoColors.LIGHT_SALMON3}] is a powerful financial data platform for supporting your investment decisions.
-
-            [bold {FinoColors.DEEP_PINK3}]Features:[/bold {FinoColors.DEEP_PINK3}]
-            - raw data ingestion workflow.
-            - data-lakehouse management.
-
-            [{FinoColors.GOLD3}]***[/{FinoColors.GOLD3}] please check --help option what you can do with fino cli [{FinoColors.GOLD3}]***[/{FinoColors.GOLD3}]
-            """,
-            title="🚀 Welcome Fino CLI",
-            border_style=FinoColors.GOLD3,
+            fino_logo,
+            border_style="panel.border",
+            title="welcome",
+            subtitle="Financial Data Management CLI Tool",
         )
     )
+
+    raise typer.Exit()
+
+
+# @app.callback(invoke_without_command=True)
+# # def callback(ctx: typer.Context) -> None:
+# #     # コマンドが指定されている場合はパネルを表示しない
+# #     if ctx.invoked_subcommand is not None:
+# #         return
+
+# #     # コマンドなしで実行された場合のみオンボーディングパネルを表示
+# #     console.print(
+# #         Panel.fit(
+# #             f"""[bold {FinoColors.ORANGE3}]Fino CLI[/bold {FinoColors.ORANGE3}]
+# #             - Financial data management CLI tool -
+# #             [bold {FinoColors.LIGHT_SALMON3}]Fino[/bold {FinoColors.LIGHT_SALMON3}] is a powerful financial data platform for supporting your investment decisions.
+
+# #             [bold {FinoColors.DEEP_PINK3}]Features:[/bold {FinoColors.DEEP_PINK3}]
+# #             - raw data ingestion workflow.
+# #             - data-lakehouse management.
+
+# #             [{FinoColors.GOLD3}]***[/{FinoColors.GOLD3}] please check --help option what you can do with fino cli [{FinoColors.GOLD3}]***[/{FinoColors.GOLD3}]
+# #             """,
+# #             title="🚀 Welcome Fino CLI",
+# #             border_style=FinoColors.GOLD3,
+# #         )
+# #     )
 
 
 if __name__ == "__main__":
